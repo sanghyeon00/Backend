780fbf2a6134a684608c9f7d5248fc9761bc6f4a
minju123486 <minju123486@gmail.com>
2024-03-24 19:58:21 +0900

    gpt and django

--- myapp/views.py
+++ myapp/views.py
@@ -3,14 +3,45 @@ from django.shortcuts import render, HttpResponse, redirect
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-# Create your views here.
+#import openai
+#import os
 
 
 user_data = dict()
 
 
+from django.shortcuts import render 
+from django.http import JsonResponse 
+import openai
+import os
+
+openai.api_key='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' 
+
+history = []
+
+def get_completion(prompt): 
+   print(prompt)
+   history.append({'role':'user','content':prompt}) 
+   query = openai.ChatCompletion.create( 
+      model="gpt-4",
+      messages=history, 
+      max_tokens=1024, 
+      n=1, 
+      stop=None, 
+      temperature=0.5, 
+   ) 
+   response = query.choices[0].message["content"]
+   history.append({'role':'assistant', 'content':response})
+   print(response) 
+   return response 
 def index(request):
     return HttpResponse("Communication start")
+    
+def query_view(request): 
+    prompt = request.data.get('username') 
+    prompt=str(prompt)
+    response = get_completion(prompt)
+    return JsonResponse({'response': response}) 
 
 @api_view(['POST'])
 def login_view(request):
@@ -19,6 +50,8 @@ def login_view(request):
     password = request.data.get('password')
     check = True
     print(username)
+    a = query_view(request)
+    print(a)
     if username not in user_data:
         user_data[username] = password
         check = True
