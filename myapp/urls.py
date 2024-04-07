e444d25d3720e8c14d0305788586195475e2744b
minju123486 <minju123486@gmail.com>
2024-04-07 21:17:09 +0900

    Prompt and system

--- myapp/urls.py
+++ myapp/urls.py
@@ -20,5 +20,5 @@ from myapp import views
 urlpatterns = [
     path('', views.index),
     path('login/', views.login_view),
-    path('GenerateProblem/', views.GenerateProblem)
+    path('GenerateProblem/', views.GenerateWriteProblem)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -11,28 +11,18 @@ user_data = dict()
 
 
 from django.shortcuts import render 
-from django.http import JsonResponse 
+from django.http import JsonResponse
+from myapp import gpt_prompt
 import openai
 import os
 
-das = 3
 
 history = []
-def time(request):
-    return 3
-System_message = '''
-우리가 문제를 만들어달라고 할 때, '문제' 키워드를 그대로 적어주고 문제가 몇 번째인지는 적지마 '문제1'에서 1 같은 키워드는 없어야해 그리고 그 옆에 반드시 스페이스 두 칸을 띄우고 문제 내용을 적어줘. 이 부분은 한 줄로 처리가 돼야해 그리고 split("  ")으로 구분이 될 수 있게 반드시 문제와 문제 내용 사이에는 두 칸을 띄워줘.
-우리가 문제를 만들어달라고 할 때, 문제와 관련된 부분을 제외한 추가적인 대답을 원하지 않아. (ex 네 알겠습니다)
-객관식 문제를 만들어달라고 할 때는, 4지선다로만 내주고 선택지마다 1.) 같은 키워드로 순서를 구분해줘 반드시 숫자마다 .) 가 있어야해.
-문제를 여러개 만들어 달라고 할 때는 문제와 문제사이는 반드시 '----------------' 출력해줘 이거를 제외하고는 아무것도 없어야해 공백이든 엔터든.
-'''
-mutiple_question = '''if we want you make problem, you always make five-choice question.'''
-write_question = '''if we want you make problem, you always make one Short answer question'''
 def get_completion(prompt):     
-    history.append({'role':'user','content':prompt}) 
+    history.append({'role':'user','content':gpt_prompt.prompt_1}) 
     query = openai.ChatCompletion.create( 
        model="gpt-4",
-       messages=[{"role": "system", "content": System_message}, {'role':'user','content':"자바 객관식 문제 만들어줘"}], 
+       messages=[{"role": "system", "content": gpt_prompt.Short_answer_message}, {'role':'user','content':gpt_prompt.prompt_4}], 
        max_tokens=1024, 
        n=1,
        stop=None,
@@ -74,9 +64,46 @@ def login_view(request):
     else:
         # �α��� ���� ó��
         return Response({'message': 'Fail'}, status=status.HTTP_401_UNAUTHORIZED)
-    
+
+
 @api_view(['POST'])
-def GenerateProblem(request):
+def GenerateWriteProblem(request):
+    a, tmp = query_view(request)
+    lst = list(tmp.split("\n"))
+    rtr = []
+    check = False
+    tempt = dict()
+    for i in lst:
+        if check:
+            id = 0
+            while i[id] == ' ' or i[id] == '.' or i[id] == '1' or i[id] == '2' or i[id] == '3' or i[id] == '4':
+                id += 1 
+            tempt['question'] = i[id::]
+            rtr.append(tempt)
+            tempt = dict()
+            check = False
+            continue
+        if i[0:2] == '문제':
+            if len(i) == 2:
+                check = True
+                continue
+            else:
+                id = 2
+                while id <len(i) and i[id] == ' ':
+                    id += 1
+                if len(i) == id:
+                    check = True
+                    continue
+                b = i[id::]
+                tempt['question'] = b
+                rtr.append(tempt)
+                tempt = dict()
+    print(rtr)
+    return Response(rtr, status=status.HTTP_200_OK)
+                
+
+@api_view(['POST'])
+def GenerateMultipleProblem(request):
     a, tmp = query_view(request)
     lst = list(tmp.split("\n"))
     rtr = []
@@ -85,26 +112,34 @@ def GenerateProblem(request):
     tempt = dict()
     for i in lst:
         if check:
-            tempt['question'] = i
+            id = 0
+            while i[id] == ' ' or i[id] == '.' or i[id] == '1' or i[id] == '2' or i[id] == '3' or i[id] == '4':
+                id += 1 
+            tempt['question'] = i[id::]
+            check = False
             continue
         if i[0:2] == '문제':
             if len(i) == 2:
                 check = True
             else:
-                b = i[5::]
+                id = 2
+                while id <len(i) and i[id] == ' ':
+                    id += 1
+                if len(i) == id:
+                    check = True
+                    continue
+                b = i[id::]
                 tempt['question'] = b
                 tempt['options'] = []
-        elif len(i) >= 0 and i[0]!= '-' and 1 <= int(i[0]) <= 4:
+        elif len(i) > 0 and i[0]!= '-' and i[0] != ' ' and i[0:2] != '정답' and 1 <= int(i[0]) <= 4:
             b = i[4:]
             tempt['options'].append(b)
-            if int(i[0]) == 4:
-                tempt['answer'] = 'A'
-                rtr.append(tempt)
-        else:
+        elif i[0:2] == '정답':
+            tempt['answer'] = i
+            for j in i:
+                if i == '1' or i == '2' or i == '3' or i == '4' :tempt['answer_number'] = int(i)
+            rtr.append(tempt)
             tempt = dict()
             continue
-            
-            
-            
     print(rtr)
     return Response(rtr, status=status.HTTP_200_OK)
