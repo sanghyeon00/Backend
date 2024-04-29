a09cd5b1cfea368c902085df50b9e58c130cadfc
minju123486 <minju123486@gmail.com>
2024-04-30 03:00:07 +0900

    sing together

--- daily/views.py
+++ daily/views.py
@@ -16,9 +16,10 @@ from django.http import JsonResponse
 from myapp import gpt_prompt
 import openai
 import os
-env = environ.Env()
-environ.Env.read_env(Path(__file__).resolve().parent/'.env')
-openai.api_key = env('Key')
+# env = environ.Env()
+# environ.Env.read_env(Path(__file__).resolve().parent/'.env')
+# openai.api_key = env('Key')
+openai.api_key = "sk-proj-CmO2Py1IeXWxZ33nc9KmT3BlbkFJL5UqyI6o06B6iFXvxnqQ"
 def generate_blog(who, what, why, where, when, how, prompt_korean_template):
     model_engine = "gpt-3.5-turbo"
     prompt_korean = prompt_korean_template.format(
